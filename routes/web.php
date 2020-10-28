17aa3a5d2c516599f2104cdaab6c9f5f60f22197
wang zhen yu <asdfg10510588@gmail.com>
2020-10-29 00:29:59 +0800

    練習 2-1：修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('/',function(){
-    return'welcome';
+    return 'welcome';
 });
 
 Route::get('r1', function(){ return redirect('r2'); });
 Route::get('r2', function(){ return view('welcome'); });
+
+Route::get('hello/{name}',function($name){
+    return 'Hello,'.$name;
+});
